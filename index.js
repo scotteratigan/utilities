bd12eeabd38de46b9f887156c376c8dde3159f92
Scott Ratigan <36186544+scotteratigan@users.noreply.github.com>
2019-10-11 14:02:11 -0700

    Added jsonToCSV

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ function deepFreeze(object) {
   return Object.freeze(newObj);
 }
 
+function jsonToCSV(data) {
+  /*
+  Takes a JSON array and converts it to a csv string
+  
+  input:
+  {[
+  {id: 1, name: "Jim"},
+  {id: 2, name: "Joe"},
+  {id: 3, name: "Jaime"}
+  ]}
+  
+  output:
+  id,name
+  1,Jim
+  2,Joe
+  3,Jaime
+  
+  Note: does not currently escape commas in data.
+  */
+  
+  return (
+    Object.keys(data[0]).join(",") +
+    "\n" +
+    data.map(obj => Object.values(obj).join(",")).join("\n")
+  );
+}
+
 module.exports = {
   deepFreeze
 };
